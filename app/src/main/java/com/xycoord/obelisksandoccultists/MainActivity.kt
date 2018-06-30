7b17ea2acb8f77c4a40aa194a2962e8d0b47e113
TheSubVisualDev <32463497+TheSubVisualDev@users.noreply.github.com>
2018-06-30 21:25:43 +0100

    Added a lot of stuff including some basic code.

--- app/src/main/java/com/xycoord/obelisksandoccultists/MainActivity.kt
+++ app/src/main/java/com/xycoord/obelisksandoccultists/MainActivity.kt
@@ -2,11 +2,35 @@ package com.xycoord.obelisksandoccultists
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.widget.NestedScrollView
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById(R.id.myHoveringButton) as FloatingActionButton
+        val myText = findViewById(R.id.myText) as TextView
+        val drawer = findViewById(R.id.nestedScrollView) as NestedScrollView
+        val drawerButtonTemp = findViewById(R.id.myHoveringButton2) as FloatingActionButton
+
+        myButton.setOnClickListener {
+            if(myText.text.toString() == getString(R.string.test))
+                myText.text = getString(R.string.test2)
+            else
+                myText.text = getString(R.string.test)
+        }
+
+        drawerButtonTemp.setOnClickListener {
+            if(drawer.visibility == View.VISIBLE)
+                drawer.visibility = View.GONE
+            else
+                drawer.visibility = View.VISIBLE
+        }
+
     }
 }
